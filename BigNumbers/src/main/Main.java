8abe72c5eb1b76be5cdd83be2c1bd094a539f0bc
hufftd <hufftd@IL-PC-153.uccc.uc.edu>
2018-01-23 20:43:18 -0500

    Second commit

--- BigNumbers/src/main/Main.java
+++ BigNumbers/src/main/Main.java
@@ -1,10 +1,23 @@
+/*
+ * Name: 
+ * Assignment #: Assignment0X
+ * Due Date: X-XX-XX
+ * Course / Section / Semester: IT2045C / 001 / Spring 2018
+ * Citations: X
+ * Description: XXXXX
+ * Anything Else Relevant: 
+ * Last Worked on: X-XX-XX
+ */
 package main;
+
+import primeCal.PrimeCal;
 
 public class Main {
 
 	public static void main(String[] args) {
+		PrimeCal pc = new PrimeCal();
 		
-
+		
 	}
 
 }
--- BigNumbers/src/primeCal/PrimeCal.java
+++ BigNumbers/src/primeCal/PrimeCal.java
@@ -1,5 +1,22 @@
+/*
+ * Name: Timothy Huff
+ * Assignment #: Assignment03
+ * Due Date: 1-30-18
+ * Course / Section / Semester: IT2045C / 001 / Spring 2018
+ * Citations: none
+ * Description: This program calculates whether or not a number is prime.
+ * Anything Else Relevant: 
+ * Last Worked on: X-XX-XX
+ */
 package primeCal;
 
+/**
+ * This Class provides the calculator for the program to determine whether or not a number is prime.
+ * @author hufftd
+ *
+ */
+
 public class PrimeCal {
+	
 
 }
